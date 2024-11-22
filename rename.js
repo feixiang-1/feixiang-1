03b36dd5524599721878b4ff82d3b9f6cef86ac9
feeeeeeeeeeeee <xxbing@xxbing.com>
2024-11-22 08:52:45 +0800

    Update rename.js

--- rename.js
+++ rename.js
@@ -292,7 +292,12 @@ function operator(pro) {
   numone && oneP(pro);
   blpx && (pro = fampx(pro));
   key && (pro = pro.filter((e) => !keyb.test(e.name)));
-  return pro;
+  newProxies = pro.map(proxie => 
+    proxie.type === 'hysteria2'
+      ? { ...proxie,  up: "120 Mbps", down: "600 Mbps" }
+      : { ...proxie }
+  );
+  return newProxies;
 }
 
 // prettier-ignore
