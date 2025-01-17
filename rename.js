788caf51cbebeddfc9f51226aad8b07eb89ba745
Fei Xiang <github@gamedb.vip>
2025-01-17 20:32:45 +0800

    fix code

--- rename.js
+++ rename.js
@@ -292,12 +292,13 @@ function operator(pro) {
   numone && oneP(pro);
   blpx && (pro = fampx(pro));
   key && (pro = pro.filter((e) => !keyb.test(e.name)));
-  newProxies = pro.map(proxie => 
+  pro = pro.map(proxie => 
     proxie.type === 'hysteria2'
       ? { ...proxie,  up: "120 Mbps", down: "600 Mbps" }
       : { ...proxie }
   );
-  return newProxies;
+  pro = pro.filter(item => item.type !== "ss" || "cipher" in item);
+  return pro;
 }
 
 // prettier-ignore
